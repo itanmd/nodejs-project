096c75e142a644632ef69fc614c808c4542e223f
Itan <itanmoshe@gmail.com>
2023-06-07 22:08:40 +0300

    connect to db

--- routes/api/api.js
+++ routes/api/api.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const usersRouter = require("./users");
 const animalsRouter = require("./animals");
+const authRouter = require("./auth");
 
 // http://localhost:3001/api/
 router.get("/", (req, res) => {
   res.json({ msg: "hello!" });
 });
-// http://localhost:3001/api/animals
+// // http://localhost:3001/api/animals
 // router.get("/animals", (req, res) => {
 //   res.json(["dog", "unicorn", "dragon", "bigfoot", "rahno"]);
 // });
@@ -17,5 +18,8 @@ router.use("/users", usersRouter);
 
 //http://localhost:3001/api/animals
 router.use("/animals", animalsRouter);
+
+//http://localhost:3001/api/auth
+router.use("/auth", authRouter);
 
 module.exports = router;
