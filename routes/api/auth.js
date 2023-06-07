d059c2af07b4b7ac25d77e13163bebbf6947a1c8
Itan <itanmoshe@gmail.com>
2023-06-07 23:08:40 +0300

    joi

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,15 +4,22 @@ const usersModule = require("../../models/users.model");
 
 router.post("/signup", async (req, res) => {
   try {
-    const user = await usersModule.insertUser(
-      "kenny",
-      "mc",
-      "kenny@gmail.com",
-      "123",
-      "050"
-    );
-    console.log("user", user);
+
   } catch (err) {}
 });
+
+// router.post("/signup", async (req, res) => {
+//     try {
+//       const user = await usersModule.insertUser(
+//         "kenny",
+//         "mc",
+//         "kenny@gmail.com",
+//         "123",
+//         "050"
+//       );
+//       console.log("user", user);
+//     } catch (err) {}
+//   });
+  
 
 module.exports = router;
